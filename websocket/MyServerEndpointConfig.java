ac730c49a9971db74d791711b96d1d1f36797e1b
javellerr <51513051+javellerr@users.noreply.github.com>
2020-06-09 23:16:11 +0900

    Update MyServerEndpointConfig.java
    주석 추가

--- websocket/MyServerEndpointConfig.java
+++ websocket/MyServerEndpointConfig.java
@@ -2,6 +2,7 @@ import javax.websocket.HandshakeResponse;
 import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerEndpointConfig;
 
+// Get end point config from http header
 public class MyServerEndpointConfig extends ServerEndpointConfig.Configurator {
 	@Override
 	public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
